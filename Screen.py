83fa2c6b2dbddbbbea960e03b12ab00e700b5d96
Emerson Maki <Emerson.Maki@altec.com>
2019-03-09 09:55:26 -0600

    Fixed the gap by simply drawing a line between the previous mouse position and the new mouse position. Also added a simple cursor.

--- Screen.py
+++ Screen.py
@@ -9,42 +9,43 @@ import math
 import random
 
 
-def draw_small_circle(screen, x, y, key):
-    pg.draw.circle(screen, GREEN, (x, y), 10, 10)
+def draw_small_circle(pos, key, radius):
+    pg.draw.circle(screen, GREEN, pos, radius, 1)
     if key:
-        pg.draw.circle(screen, RED, (x, y), 5, 5)
+        pg.draw.circle(screen, RED, pos, 5, 5)
 
 
 def distance(start, end):
-
     dist = 0
     if len(start) == len(end) == 1:
         dist = abs(start - end)
     elif len(start) == len(end) == 2:
         # two points, (x1,y1) (x2,y2)
         # abs(x1 - x2)**2 + abs(y1 - y2)**2 = dist**2
-        dist = math.sqrt(abs(start[0] - end[0]) ** 2 +
-                         abs(start[1] - end[1]) ** 2)
+        dist = math.sqrt((abs(start[0] - end[0]) ** 2) +
+                         (abs(start[1] - end[1]) ** 2))
     return dist
 
 
 pg.init()
 
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-GREEN = (0, 255, 0)
-RED = (255, 0, 0)
-BLUE = (0, 0, 255)
-JENNAS = (153, 50, 204)
+BLACK =  (0  , 0  , 0  )
+WHITE =  (255, 255, 255)
+GREEN =  (0  , 255, 0  )
+RED =    (255, 0  , 0  )
+BLUE =   (0  , 0  , 255)
+JENNAS = (153, 50 , 204)
 
 
-sizeX = 700
-sizey = 500
-size = [sizeX, sizey]
+size_X = 700
+size_Y = 500
+size = [size_X, size_Y]
+background_color = BLACK
 
 screen = pg.display.set_mode(size)
+screen.fill(background_color)
 pg.display.set_caption('Functions')
-pg.mouse.set_visible(False)
+pg.mouse.set_cursor(*pg.cursors.ball)
 
 done = False
 clock = pg.time.Clock()
@@ -52,11 +53,12 @@ key_F = False
 key_J = False
 key_C = False
 
-mouse_previous_pos = []
+mouse_previous_pos = pg.mouse.get_pos()
 
 while not done:
     mouse_press = pg.mouse.get_pressed()
     mouse_current_pos = pg.mouse.get_pos()
+    mouse_distance = distance(mouse_current_pos, mouse_previous_pos)
 
     for event in pg.event.get():
         if event.type == pg.QUIT:
@@ -79,31 +81,24 @@ while not done:
                 key_C = False
 
     if mouse_press[0]:
-        draw_small_circle(screen, mouse_current_pos[0], mouse_current_pos[1], key_F)
-        circleDistance = distance(mouse_current_pos, mouse_previous_pos)
-        if circleDistance > 2:
-            incrementDistance = circleDistance/10
-            for val in range(math.ceil(circleDistance/10)):
-                draw_small_circle(screen,
-                                  mouse_current_pos[0]+val,
-                                  mouse_current_pos[1]+val,
-                                  key_F)
+        radius = 3
+        # draw_small_circle(mouse_current_pos, key_F, radius)
+        pg.draw.line(screen, GREEN, mouse_previous_pos, mouse_current_pos, radius)
 
     if key_J:
-        pg.draw.circle(screen,
-                       JENNAS,
-                       (random.randint(0, sizeX), random.randint(0, sizey)),
+        pg.draw.circle(screen,JENNAS,
+                       (random.randint(0, size_X), random.randint(0, size_Y)),
                        random.randint(40, 100),
                        random.randint(0, 39)
                        )
 
     if key_C:
-        screen.fill(BLACK)
+        screen.fill(background_color)
 
-    previous_mouse_pos = mouse_current_pos
+    mouse_previous_pos = mouse_current_pos
     pg.display.flip()
 
-    clock.tick(160)
+    clock.tick(60)
 
 
 pg.quit()
